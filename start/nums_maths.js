5c86d7490ef7d0cd88c40a4ca12c9e923af04ad0
UjalaShrivastava <146463972+UjalaShrivastava@users.noreply.github.com>
2025-02-23 18:32:19 +0000

    done with changes

---
+++ start/nums_maths.js
@@ -0,0 +1,12 @@
+//----------------NUMBERS-------------
+
+const score = 400;
+console.log(score);
+
+console.log(score.toString());
+console.log(typeof(score));
+
+const amount = 1000000;
+console.log(amount.toLocaleString('en-IN'));
+
+//---------------MATHS-----------------
