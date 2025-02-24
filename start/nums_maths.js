ac5dadb6867f77a2ae3e6e6c67e7a123ea14d5eb
UjalaShrivastava <146463972+UjalaShrivastava@users.noreply.github.com>
2025-02-24 13:16:41 +0000

    changes done

--- start/nums_maths.js
+++ start/nums_maths.js
@@ -10,3 +10,16 @@ const amount = 1000000;
 console.log(amount.toLocaleString('en-IN'));
 
 //---------------MATHS-----------------
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(5.7));
+console.log(Math.floor(7.9));
+console.log(Math.ceil(6.1));
+console.log(Math.min(5,3,9,0));
+console.log(Math.max(5,3,9,0));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
