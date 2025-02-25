3d5e11886bf67ae0e3299041ee162cc2c2e0dec3
UjalaShrivastava <146463972+UjalaShrivastava@users.noreply.github.com>
2025-02-25 19:11:23 +0000

    changes done

---
+++ part2/objects2.js
@@ -0,0 +1,41 @@
+const employee = {}
+
+employee.id = "12345",
+employee.name = "Raj",
+employee.age = 27
+
+console.log(employee);
+console.log(Object.keys(employee));
+console.log(Object.values(employee));
+console.log(Object.entries(employee));
+console.log(employee.hasOwnProperty('id'));
+
+const managers = {
+    email: "hello@.com",
+    fullname: {
+        usersfullname: {
+            firstname: "Shain",
+            lastname: "Roy"
+        }
+    }
+}
+
+// console.log(managers.fullname.usersfullname.lastname);
+
+const car = {
+    brand: "TATA",
+    model: "Thar"
+}
+
+const flower2 = {
+    name: "Tulip",
+    color: "Pink"
+}
+
+const hii = {a: "1", b: "3"}
+
+// const things = {car, flower2}
+// const things = Object.assign({}, car, flower2, hii)
+const things = {...car, ...flower2, ...hii} 
+// console.log(things);
+
