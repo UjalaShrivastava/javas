a9f32368997e010031f7d0996652fe1a259f862e
UjalaShrivastava <146463972+UjalaShrivastava@users.noreply.github.com>
2025-02-25 12:04:51 +0000

    done with changes

---
+++ part2/arrays.js
@@ -0,0 +1,26 @@
+const fruits = ["Mango", "Grapes", "papaya", "Apple"];
+const flowers = ["Tulip", "Lotus", "Rose", "Sunflower"];
+
+const nfruits = fruits.slice(0, 2);
+console.log(nfruits);
+
+const nflowers = flowers.splice(1, 2);
+console.log(nflowers);
+
+
+// fruits.push(flowers);
+// console.log(fruits);
+
+// const all = fruits.concat(flowers)
+// console.log(all);
+
+// const total = [...fruits, ...flowers];
+// console.log(total);
+
+// const array = [1, 2, 3, 4, [4, 5, 0], 9, 6, [9, 3, 8, [2, 4, 0]]]
+// const new_array = array.flat(Infinity)
+// console.log(new_array);
+
+console.log(Array.isArray("Ujala"));
+console.log(Array.from("Ujala"));
+
