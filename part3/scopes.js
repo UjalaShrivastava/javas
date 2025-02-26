f01357f1e18bb06eac10cfc2154b63cef0a2fbee
UjalaShrivastava <146463972+UjalaShrivastava@users.noreply.github.com>
2025-02-26 18:23:45 +0000

    done

---
+++ part3/scopes.js
@@ -0,0 +1,13 @@
+let a = 200
+
+if (true){
+    let a = 30;
+    const b = 299
+    // console.log(`value of a in the local scope is: ${a}`);
+    
+}
+
+// console.log(`value of a in the global scope is: ${a}`);
+
+
+
