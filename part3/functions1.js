8ab07435e14db832e65a51e1449deea2d5b1ac6c
UjalaShrivastava <146463972+UjalaShrivastava@users.noreply.github.com>
2025-02-26 18:02:11 +0000

    done with changes

--- part3/functions1.js
+++ part3/functions1.js
@@ -32,5 +32,41 @@ function loginUserMsg(username){
     }
     return `${username} just logged in`
 }
+// console.log(loginUserMsg("hello"));
 
-console.log(loginUserMsg());
+
+
+function cartPrice(...num1){
+    return num1
+}
+// console.log(cartPrice(200, 100, 899));
+
+
+//passing object in function-----------------
+
+const dress = {
+    dreesType: "Shirt",
+    price: 999,
+    color: "olive green"
+}
+
+function handleObject(anyobject){
+    console.log(`dresstype is ${anyobject.dreesType} and price of this dress is ${anyobject.price}`);
+}
+// handleObject(dress)
+handleObject({
+    dreesType: "Jeans",
+    price: 1999
+})
+
+
+
+// passing arrays in function
+
+const arr = [399, 499, 699, 599]
+
+function newArr(getArr){
+    return getArr[1]
+}
+
+console.log(newArr(arr))
