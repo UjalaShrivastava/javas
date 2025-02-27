333fb4a5a122ca497a22e87b8a3482441e18f091
UjalaShrivastava <146463972+UjalaShrivastava@users.noreply.github.com>
2025-02-27 07:27:14 +0000

    changes done

--- part3/scopes.js
+++ part3/scopes.js
@@ -11,3 +11,45 @@ if (true){
 
 
 
+function one(){
+    const name = "Ujala"
+
+    function two(){
+        const address = "Motihari"
+        console.log(name);    
+    }
+    // console.log(address);
+    two()
+}
+one()
+
+
+
+if (true) {
+    const username = "Shalu"
+    if(username == "Shalu"){
+        const website = " Google"
+        console.log(username + website);  
+    }
+    // console.log(website);   
+}
+// console.log(username);
+
+
+
+//------------------INTERESTING--------------------
+
+console.log(addone(5))
+function addone(num) {                   // function  //only declaration
+    return num + 1
+}
+
+
+
+//hoisting------------
+
+const addTwo = function(num){           // can say function expression  //declared by holding in the variable
+    return num + 2
+}
+console.log(addTwo(7));
+
