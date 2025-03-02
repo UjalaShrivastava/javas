a83d1f792fbb3affb89492d97e7f18571d0cb358
UjalaShrivastava <146463972+UjalaShrivastava@users.noreply.github.com>
2025-03-02 16:54:55 +0000

    changes done

---
+++ controlflow/first.js
@@ -0,0 +1,19 @@
+// if----------------
+
+// if (condition){}  ---(condition should be true to run the code)
+
+const a = 10;
+if (a === 10){
+    console.log("Yes it is");
+}
+console.log("hello...done");
+
+
+// if-else---------------
+
+const b = 600;
+if (b === 400){
+    console.log("Yes it is");
+}else{
+    console.log("No it's false");
+}
