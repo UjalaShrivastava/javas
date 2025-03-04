40612ed9231f0ab44edd15d91b0b6ceddd1ef849
UjalaShrivastava <146463972+UjalaShrivastava@users.noreply.github.com>
2025-03-04 17:13:06 +0000

    some changes done

---
+++ part1/3string.js
@@ -0,0 +1,16 @@
+const name = "ujala";
+const age = 21;
+
+console.log(name + age + " done");
+console.log(`My name is ${name} and i am ${age} years old `);
+
+console.log(name[0]);
+console.log(name.toUpperCase());
+console.log(name.charAt(4));
+console.log(name.indexOf('l'));
+
+const strings = "     cgfrtnkkhtfb     ";
+console.log(strings);
+console.log(strings.trim());
+
+
