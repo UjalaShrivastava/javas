6fa8a99b291f5d1641475d6b4eaa9dcb465c07cc
UjalaShrivastava <146463972+UjalaShrivastava@users.noreply.github.com>
2025-03-05 10:46:53 +0000

    done

---
+++ 5loops/6filter.js
@@ -0,0 +1,25 @@
+// FILTER-----------------------------------
+
+const colours = ["Red", "Yellow", "White", "Blue", "Pink"]
+
+
+// const values = colours.forEach( (item) => {                         // foreach does not return any value
+//     console.log(item);
+// })
+// console.log(values);
+
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8]
+// const newNums = nums.filter( (num) => num>3 )
+// console.log(newNums);
+
+
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8]
+// const newNums = []
+// nums.forEach( (num) => {
+//     if (num>3) {
+//         newNums.push(num)
+//     }
+// } )
+// console.log(newNums);
